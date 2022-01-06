33ed301579445e8ee58d0a011f0373fc898778f9
Mathias Oterhals Myklebust <mathias@oterbust.no>
2022-01-06 11:14:12 +0100

    Update app.py to use YAML config

--- app.py
+++ app.py
@@ -3,7 +3,20 @@ import os
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from config import Config
+
 app = FastAPI()
 
-YIELD_DIRECTORY = os.environ['PASJONSFRUKT_YIELD_DIRECTORY']
-app.mount(f"/{YIELD_DIRECTORY}", StaticFiles(directory=YIELD_DIRECTORY), name="static")
+conf = Config.from_config_file(
+    path="config/config.yaml",
+    base_path="config/base_config.yaml"
+)
+
+# Use absolute path to avoid dependence on working directory
+absolute_yield_dir = os.path.join(os.path.dirname(__file__), conf.yield_dir)
+
+# Make sure yield directory exists
+os.makedirs(absolute_yield_dir, exist_ok=True)
+
+# Mount yield directory as static files to yield path
+app.mount(f"/{conf.yield_dir}", StaticFiles(directory=absolute_yield_dir), name="static")
