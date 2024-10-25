ad187898c394c4eb241bd3ef527cccf276a911da
Mathias Oterhals Myklebust <mathias@oterbust.no>
2024-10-25 13:34:33 +0200

    fix: update podme_api to fix schibsted login

--- pasjonsfrukt/main.py
+++ pasjonsfrukt/main.py
@@ -2,32 +2,29 @@ import os
 import re
 from pathlib import Path
 
+from podme_api import PodMeDefaultAuthClient, PodMeUserCredentials, PodMeClient, PodMeEpisode
 from rfeed import Item, Guid, Enclosure, Feed, Image, iTunesItem, iTunes
-import podme_api
-import podme_api.types
-from podme_api.exceptions import AccessDeniedError
 
 from .config import Config
 from .utils import date_of_episode
 
 
 def get_podme_client(email: str, password: str):
-    client = podme_api.PodMeSchibstedClient(
-        email=email,
-        password=password
+    return PodMeClient(
+        auth_client=PodMeDefaultAuthClient(
+            user_credentials=PodMeUserCredentials(
+                email=email,
+                password=password
+            )
+        )
     )
-    try:
-        client.login()
-        return client
-    except AccessDeniedError:
-        print("[FAIL] Access denied when retrieving PodMe token, please check your login credentials")
 
 
-def harvest_podcast(client: podme_api.PodMeClient, config: Config, slug: str):
+async def harvest_podcast(client: PodMeClient, config: Config, slug: str):
     if slug not in config.podcasts:
         print(f"[FAIL] The slug '{slug}' did not match any podcasts in the config file")
         return
-    published_ids = client.get_episode_ids(slug)
+    published_ids = await client.get_episode_ids(slug)
     if len(published_ids) == 0:
         print(f"[WARN] Could not find any published episodes for '{slug}'")
         return
@@ -38,7 +35,7 @@ def harvest_podcast(client: podme_api.PodMeClient, config: Config, slug: str):
         relevant_harvest_ids = []
     else:
         relevant_harvest_ids = published_ids[-most_recent_episodes_limit:]
-    harvested_ids = harvested_episode_ids(client, config, slug)
+    harvested_ids = await harvested_episode_ids(client, config, slug)
     to_harvest = [e for e in relevant_harvest_ids if e not in harvested_ids]
     if len(to_harvest) == 0:
         print(f"[INFO] Nothing new from '{slug}', all available episodes already harvested"
@@ -52,19 +49,19 @@ def harvest_podcast(client: podme_api.PodMeClient, config: Config, slug: str):
     os.makedirs(podcast_dir, exist_ok=True)
     # harvest each missing episode
     for episode_id in to_harvest:
-        client.download_episode(
+        await client.download_episode(
             str((podcast_dir / f"{episode_id}.mp3").as_posix()),  # path must be of type str
-            client.get_episode_info(episode_id)['streamUrl']
+            (await client.get_episode_info(episode_id)).stream_url
         )
-    sync_slug_feed(client, config, slug)
+    await sync_slug_feed(client, config, slug)
 
 
-def harvested_episode_ids(client: podme_api.PodMeClient, config: Config, slug: str):
+async def harvested_episode_ids(client: PodMeClient, config: Config, slug: str):
     podcast_dir = build_podcast_dir(config, slug)
     if not podcast_dir.is_dir():
         # no directory, so clearly no harvested episodes
         return []
-    episode_ids = client.get_episode_ids(slug)
+    episode_ids = await client.get_episode_ids(slug)
     harvested = []
     for f in podcast_dir.iterdir():
         if not f.is_file():
@@ -99,26 +96,27 @@ def build_podcast_episode_file_path(config: Config, podcast_slug: str, episode_i
     return build_podcast_dir(config, podcast_slug) / f"{episode_id}.mp3"
 
 
-def build_feed(config: Config, episodes: list[podme_api.types.PodMeEpisode], slug: str, title: str, description: str,
+def build_feed(config: Config, episodes: list[PodMeEpisode], slug: str, title: str, description: str,
                image_url: str):
     secret_query_param = get_secret_query_parameter(config)
     items = []
     for e in episodes:
-        episode_id = e['id']
+        episode_id = e.id
         episode_path = f"{slug}/{episode_id}"
         items.append(Item(
-            title=e['title'],
-            description=e['description'],
+            title=e.title,
+            description=e.description,
             guid=Guid(episode_id, isPermaLink=False),
             enclosure=Enclosure(
                 url=f'{config.host}/{episode_path}/{secret_query_param}',
                 type='audio/mpeg',
                 length=build_podcast_episode_file_path(config, slug, episode_id).stat().st_size
             ),
-            pubDate=date_of_episode(e),
+            pubDate=e.date_added,
             extensions=[
                 iTunesItem(
-                    duration=e['length']
+                    author=e.author_full_name,
+                    duration=e.length,
                 )
             ]
         ))
@@ -139,20 +137,20 @@ def build_feed(config: Config, episodes: list[podme_api.types.PodMeEpisode], slu
     return feed.rss()
 
 
-def sync_slug_feed(client: podme_api.PodMeClient, config: Config, slug: str):
+async def sync_slug_feed(client: PodMeClient, config: Config, slug: str):
     if slug not in config.podcasts:
         print(f"[FAIL] The slug '{slug}' did not match any podcasts in the config file")
         return
     print(f"[INFO] Syncing '{slug}' feed...")
-    episodes = [client.get_episode_info(e) for e in harvested_episode_ids(client, config, slug)]
-    podcast_info = client.get_podcast_info(slug)
+    episodes = [await client.get_episode_info(e) for e in (await harvested_episode_ids(client, config, slug))]
+    podcast_info = await client.get_podcast_info(slug)
     feed = build_feed(
         config,
         episodes,
         slug,
-        podcast_info['title'],
-        podcast_info['description'],
-        podcast_info['imageUrl']
+        podcast_info.title,
+        podcast_info.description,
+        podcast_info.image_url
     )
     os.makedirs(build_podcast_dir(config, slug), exist_ok=True)
     with open(build_podcast_feed_path(config, slug), mode="w", encoding="utf-8") as feed_file:
