33f221a1c0a27495035d6f5247fd3eb97736f2d7
Mathias Oterhals Myklebust <mathias@oterbust.no>
2024-10-26 23:28:51 +0200

    deps: bump typer to 0.12.5

--- pasjonsfrukt/cli.py
+++ pasjonsfrukt/cli.py
@@ -1,5 +1,5 @@
 import logging
-
+from typing import Optional, Annotated
 import typer
 import uvicorn
 import pprint
@@ -16,50 +16,56 @@ cli = AsyncTyper()
 
 @cli.command()
 async def harvest(
-    podcast_slugs: list[str] = typer.Argument(
-        None,  # NB: default is actually an empty list | TODO: #108 at tiangolo/typer
-        metavar="[PODCAST_SLUG]...",
-    ),
-    config_stream: typer.FileText = typer.Option(
-        "config.yaml",
-        "--config-file",
-        "-c",
-        encoding="utf-8",
-        help="Configurations file",
-    ),
+    podcast_slugs: Annotated[
+        Optional[list[str]],
+        typer.Argument(
+            metavar="[PODCAST_SLUG]...",
+        ),
+    ] = None,
+    config_stream: Annotated[
+        Optional[typer.FileText],
+        typer.Option(
+            "--config-file",
+            "-c",
+            encoding="utf-8",
+            help="Configurations file",
+        ),
+    ] = "config.yaml",
 ):
     """
     Scrape podcast episodes
     """
     config = config_from_stream(config_stream)
     async with get_podme_client(config.auth.email, config.auth.password) as client:
-        to_harvest = (
-            config.podcasts.keys() if len(podcast_slugs) == 0 else podcast_slugs
-        )
+        to_harvest = config.podcasts.keys() if podcast_slugs is None else podcast_slugs
         for s in to_harvest:
             await harvest_podcast(client, config, s)
 
 
 @cli.command("sync")
 async def sync_feeds(
-    podcast_slugs: list[str] = typer.Argument(
-        None,  # NB: default is actually an empty list | TODO: #108 at tiangolo/typer
-        metavar="[PODCAST_SLUG]...",
-    ),
-    config_stream: typer.FileText = typer.Option(
-        "config.yaml",
-        "--config-file",
-        "-c",
-        encoding="utf-8",
-        help="Configurations file",
-    ),
+    podcast_slugs: Annotated[
+        Optional[list[str]],
+        typer.Argument(
+            metavar="[PODCAST_SLUG]...",
+        ),
+    ] = None,
+    config_stream: Annotated[
+        Optional[typer.FileText],
+        typer.Option(
+            "--config-file",
+            "-c",
+            encoding="utf-8",
+            help="Configurations file",
+        ),
+    ] = "config.yaml",
 ):
     """
     Update RSS podcast feeds to match scraped episodes
     """
     config = config_from_stream(config_stream)
     async with get_podme_client(config.auth.email, config.auth.password) as client:
-        to_sync = config.podcasts.keys() if len(podcast_slugs) == 0 else podcast_slugs
+        to_sync = config.podcasts.keys() if podcast_slugs is None else podcast_slugs
         for s in to_sync:
             await sync_slug_feed(client, config, s)
 
@@ -73,13 +79,15 @@ async def sync_feeds(
 )
 def serve_api(
     ctx: typer.Context,
-    config_stream: typer.FileText = typer.Option(
-        "config.yaml",
-        "--config-file",
-        "-c",
-        encoding="utf-8",
-        help="Configurations file",
-    ),
+    config_stream: Annotated[
+        Optional[typer.FileText],
+        typer.Option(
+            "--config-file",
+            "-c",
+            encoding="utf-8",
+            help="Configurations file",
+        ),
+    ] = "config.yaml",
 ):
     """
     Serve RSS podcast feeds
@@ -98,13 +106,15 @@ def serve_api(
 
 @cli.command(name="config")
 def print_config(
-    config_stream: typer.FileText = typer.Option(
-        "config.yaml",
-        "--config-file",
-        "-c",
-        encoding="utf-8",
-        help="Configurations file",
-    )
+    config_stream: Annotated[
+        Optional[typer.FileText],
+        typer.Option(
+            "--config-file",
+            "-c",
+            encoding="utf-8",
+            help="Configurations file",
+        ),
+    ] = "config.yaml",
 ):
     """
     Print parsed config
